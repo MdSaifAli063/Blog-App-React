c4f654586cb1a0fe317d83afc5cc5d00d576e1bb
Mdsaif4363 <mdsaifali6303@gmail.com>
2025-10-30 01:06:03 +0530

    logo added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width = "100%"}) {
   return (
-    <img src='' style={{width}} alt='Logo placeholder' />
+    <img src='https://ik.imagekit.io/77nsbwefl/sddas.png?updatedAt=1761764886975' style={{width}} alt='Logo placeholder' />
   )
 }
 
