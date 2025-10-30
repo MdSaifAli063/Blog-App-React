1532df85ed615a802cd7c728d6512f47d1267f93
Mdsaif4363 <mdsaifali6303@gmail.com>
2025-10-31 01:59:05 +0530

    updated

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import appwriteService from "../appwrite/config.js"
 
-function PostCard({
-    $id, title, featuredImage
-}) {
+function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
-        <div
-        className='w-full bg-gray-100 rounded-xl p-4'
-        >
-            <div
-            className='w-full justify-center mb-4'
-            >
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl'
-                />
-            </div>
-            <h2 className='text-xl font-bold'>{title}</h2>
+      <div className="w-full bg-white/60 backdrop-blur-md rounded-xl p-4 shadow-md hover:shadow-lg transition duration-300">
+        <div className="w-full flex justify-center mb-4">
+          <img
+            src={appwriteService.getFilePreview(featuredImage)}
+            alt={title}
+            className="rounded-xl w-full h-48 object-cover"
+          />
         </div>
+        <h2 className="text-xl font-bold text-gray-800 text-center">{title}</h2>
+      </div>
     </Link>
   )
 }
